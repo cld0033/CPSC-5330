8ba78854174125cdc25b66c7745e3a63cb7f79e6
cld0033 <cld0033@auburn.edu>
2026-01-11 17:26:23 -0600

    Add files via upload

---
+++ CLD0033_Assignment1/CLD0033_Assignment1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CLD0033_Assignment1
+//
+//  Created by Chester Davidson on 1/11/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
